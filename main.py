654018ed8dce665aafea53f6a9f360ef0f6c04e6
20sampaioeggintonw <20SampaioEggintonW@jmf6.org>
2026-05-19 09:48:17 +0100

    new values added and it dumps to a file now "infoSet.txt"

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import json
 number_plate = str(input("whats the number plate"))
 parking_duration = int(input("how many hours do you want to park"))
 
+paymount_amount = (parking_duration * 2) - 0.01
 print(f"£{(parking_duration * 2) - 0.01}")
 
 infoSet = {"plate":number_plate,
            "duration":parking_duration,
            "paid":False,
            "inParkingArea":False,
+           "chargeDue":paymount_amount
         }
 
-infoSet = json.dumps(infoSet)
+json.dumps(infoSet)
 
 with open("infoSet.txt", "w") as file:
     json.dump(infoSet, file)
