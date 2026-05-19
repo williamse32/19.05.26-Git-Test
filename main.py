0ca18bfb31c256ad3c7e1c9521c7818ee3d1e518
20sampaioeggintonw <20SampaioEggintonW@jmf6.org>
2026-05-19 09:38:32 +0100

    Added a lot

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def helloworld(arg):
-    print(arg)
+import json
 
+number_plate = str(input("whats the number plate"))
+parking_duration = int(input("how many hours do you want to park"))
 
-helloworld("helloworld")
-helloworld("print")
+print(f"£{(parking_duration * 2) - 0.01}")
+
+infoSet = {"plate":number_plate,
+           "duration":parking_duration,
+           "paid":False,
+           "inParkingArea":False,
+        }
+
+infoSet = json.dumps(infoSet)
+
+with open("infoSet.txt", "w") as file:
+    json.dump(infoSet, file)
